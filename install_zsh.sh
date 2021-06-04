af87da362713d3ac3d94b5d42fcb61533e7c454f
AndresMpa <andresprieto2703@gmail.com>
2021-06-04 08:15:49 -0500

    Fixing installation

--- install_zsh.sh
+++ install_zsh.sh
@@ -25,81 +25,82 @@ sudo apt-get install zsh-autosuggestions -y
 # Mejor ls
 wget mirrors.kernel.org/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
 sudo dpkg -i exa_0.9.0-4_amd64.deb
-cd .oh-my-zsh/custom/plugins
+rm -rf exa_0.9.0-4_amd64.deb
 git clone https://github.com/DarrinTisdale/zsh-aliases-exa
+mv zsh-aliases-exa .oh-my-zsh/custom/plugins
 
-#cat zsh_configuration > ~/.zshrc
+cat zsh_configuration > ~/.zshrc
 
-## Docker
+# Docker
 
-#clear
+clear
 
-#medio_de_instalacion=2
+medio_de_instalacion=2
 
-#echo "¿Como quieres instalar docker?:"
-#echo "1) Mediante respositorio"
-#echo "2) Mediante paquete"
+echo "¿Como quieres instalar docker?:"
+echo "1) Mediante respositorio"
+echo "2) Mediante paquete"
 
-#read medio_de_instalacion
+read medio_de_instalacion
 
-#if [ medio_de_instalacion == 1 ];
-	#then
-		#sudo apt-get remove docker docker-engine docker.io containerd runc
-		#sudo apt-get update
-		#sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release docker-ce docker-ce-cli containerd.io
-		#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-		#echo \
-			#"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-			#$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+if [ medio_de_instalacion == 1 ];
+	then
+		sudo apt-get remove docker docker-engine docker.io containerd runc
+		sudo apt-get update
+		sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release docker-ce docker-ce-cli containerd.io
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+		echo \
+			"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+			$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 		
-		#apt-cache madison docker-ce
+		apt-cache madison docker-ce
 		
-		#distro="lsb_release -cs"
-		#sudo apt-get install docker-ce=$distro docker-ce-cli=$distro containerd.io
-#else
+		distro="lsb_release -cs"
+		sudo apt-get install docker-ce=$distro docker-ce-cli=$distro containerd.io
+else
 
-	#if [ $distro == 'focal' ];
-	#then
-		#wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-scan-plugin_0.8.0~ubuntu-focal_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_20.10.7~3-0~ubuntu-focal_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_19.03.15~3-0~ubuntu-focal_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-rootless-extras_20.10.7~3-0~ubuntu-focal_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_20.10.7~3-0~ubuntu-focal_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_19.03.15~3-0~ubuntu-focal_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.4.6-1_amd64.deb
-	#fi
-	#if [ $distro == 'groovy' ];
-	#then
-		#wget https://download.docker.com/linux/ubuntu/dists/groovy/pool/stable/amd64/docker-scan-plugin_0.8.0~ubuntu-groovy_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/groovy/pool/stable/amd64/docker-ce_20.10.7~3-0~ubuntu-groovy_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/groovy/pool/stable/amd64/docker-ce-rootless-extras_20.10.7~3-0~ubuntu-groovy_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/groovy/pool/stable/amd64/docker-ce-cli_20.10.7~3-0~ubuntu-groovy_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/groovy/pool/stable/amd64/containerd.io_1.4.6-1_amd64.deb 
-	#fi
-	#if [ $distro == 'bionic' ]; 
-	#then
-		#wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/containerd.io_1.4.6-1_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce-cli_20.10.7~3-0~ubuntu-bionic_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce-rootless-extras_20.10.7~3-0~ubuntu-bionic_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_20.10.7~3-0~ubuntu-bionic_amd64.deb
-		#wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-scan-plugin_0.8.0~ubuntu-bionic_amd64.deb
-	#fi
-	#sudo dpkg -i *.deb
-#fi
+	if [ $distro == 'focal' ];
+	then
+		wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-scan-plugin_0.8.0~ubuntu-focal_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_20.10.7~3-0~ubuntu-focal_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_19.03.15~3-0~ubuntu-focal_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-rootless-extras_20.10.7~3-0~ubuntu-focal_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_20.10.7~3-0~ubuntu-focal_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_19.03.15~3-0~ubuntu-focal_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.4.6-1_amd64.deb
+	fi
+	if [ $distro == 'groovy' ];
+	then
+		wget https://download.docker.com/linux/ubuntu/dists/groovy/pool/stable/amd64/docker-scan-plugin_0.8.0~ubuntu-groovy_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/groovy/pool/stable/amd64/docker-ce_20.10.7~3-0~ubuntu-groovy_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/groovy/pool/stable/amd64/docker-ce-rootless-extras_20.10.7~3-0~ubuntu-groovy_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/groovy/pool/stable/amd64/docker-ce-cli_20.10.7~3-0~ubuntu-groovy_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/groovy/pool/stable/amd64/containerd.io_1.4.6-1_amd64.deb 
+	fi
+	if [ $distro == 'bionic' ]; 
+	then
+		wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/containerd.io_1.4.6-1_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce-cli_20.10.7~3-0~ubuntu-bionic_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce-rootless-extras_20.10.7~3-0~ubuntu-bionic_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_20.10.7~3-0~ubuntu-bionic_amd64.deb
+		wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-scan-plugin_0.8.0~ubuntu-bionic_amd64.deb
+	fi
+	sudo dpkg -i *.deb
+fi
 
-## Creando grupo docker
-#sudo groupadd docker
-#sudo usermod -aG docker $USER
-#newgrp docker
+# Creando grupo docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
 
-## Iniciando servicio
-#sudo systemctl enable docker.service
-#sudo systemctl enable containerd.service
+# Iniciando servicio
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
 
-#echo "Listo, si todo salio bien ahora debes tener un ambiante completo con el servicio de docker. Puedes modificar los plugins de zsh en esta ruta: 'code ~/.zshrc' \nPuede interesar estos links:"
+echo "Listo, si todo salio bien ahora debes tener un ambiante completo con el servicio de docker. Puedes modificar los plugins de zsh en esta ruta: 'code ~/.zshrc' \nPuede interesar estos links:"
 
-## Probando la instalación de docerk
-#sudo docker run hello-world
+# Probando la instalación de docerk
+sudo docker run hello-world
 
 help = "
 ZSH
