8ab5cbb0316f7dd6de965a2e9c50503f4d735727
AndresMpa <andresprieto2703@gmail.com>
2021-07-10 13:36:41 -0500

    Agregando composer

--- environment/ubuntu_en.sh
+++ environment/ubuntu_en.sh
@@ -36,7 +36,8 @@ then
 	curl -sS https://getcomposer.org/installer -o composer-setup.php
 	HASH=`curl -sS https://composer.github.io/installer.sig`
 	php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-
+	sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+	
 	# Installing MySQL & Apache
 	sudo mysql_secure_installation
 	sudo ufw app list
--- environment/ubuntu_es.sh
+++ environment/ubuntu_es.sh
@@ -36,6 +36,7 @@ then
 	curl -sS https://getcomposer.org/installer -o composer-setup.php
 	HASH=`curl -sS https://composer.github.io/installer.sig`
 	php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+	sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 	sudo mysql_secure_installation
 	sudo ufw app list
